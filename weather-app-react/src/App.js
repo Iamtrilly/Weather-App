2794eddd073702a34d09aa3fa46185dbf66b9b1f
Mishy <56826409+Iamtrilly@users.noreply.github.com>
2023-09-24 15:29:15 +0300

    Modified changes in background image and build folder.

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,9 +1,33 @@
-
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  //const url =`https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=647890075e3e577ad6d9c22a211050f7`
+
   return (
-    <div>
-      
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Nairobi</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20</p>
+          </div>
+          <div className="wind">12 MPH</div>
+        </div>
+      </div>
     </div>
   );
 }
